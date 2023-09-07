0304394937b8686ed04c95d5ce66ac53c6c8a889
khanh251204 <143423217+khanh251204@users.noreply.github.com>
2023-09-07 10:06:27 +0700

    create

---
+++ tuple/tuple.py
@@ -0,0 +1,12 @@
+#tuple đc kahi báo bằng dấu ()
+a = (1,2,3,4)
+print(a)
+# Note chỉ có tuple mới khai báo dc với int, float, string and list)
+b = (1,2,3,4,[1,2,3],'Khánh')
+print(b)
+print(type(b))
+
+matran = (1,2,3),(3,4,5)
+
+print(matran[1][1])
+
